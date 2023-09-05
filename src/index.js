1a9e7b9beb766f65e46fafb55941d174b15e0b69
ThoaTK <17521095@gm.uit.edu.vn>
2023-09-05 15:55:45 +0700

    refactor code: create Router, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 const path = require('path');
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+const route = require('./routes');
 
 app.use(express.static( path.join(__dirname, "public")))
 
+
+app.use(express.urlencoded({
+  extended: true,
+}))
+app.use(express.json())
+
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 
 //Template engine
 app.engine('.hbs', handlebars.engine(
   {
     extname: '.hbs',
-    // partialsDir  : [
-    //   //  path to your partials
-    //   path.join(__dirname, '/resources/views/partials'),
-    // ]
   }
 ));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, '/resources/views'));
 
-app.get('/', function (req, res) {
-  res.render('home');
-});
-app.get('/news', function (req, res) {
-  res.render('news');
-});
+
+//Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
